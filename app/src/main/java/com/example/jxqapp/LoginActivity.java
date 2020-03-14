a86ee90295ac8f882fbde6ac91564c6417f63eee
方艺彬 <447231887@qq.com>
2020-03-14 23:20:14 +0800

    登录

--- app/src/main/java/com/example/jxqapp/LoginActivity.java
+++ app/src/main/java/com/example/jxqapp/LoginActivity.java
@@ -3,11 +3,54 @@ package com.example.jxqapp;
 import android.app.Activity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.jxqapp.util.HttpUtil;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button btn_submit;
+    private EditText intput_username;
+    private EditText intput_password;
+    boolean isSubmit = true;
+    String username;
+    String password;
+
+    final OkHttpClient client = new OkHttpClient();
+    private Handler mHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            if(msg.what==1){
+                String ReturnMessage = (String)msg.obj;
+                Log.i("获取的返回信息",ReturnMessage);
+                /***
+                 * 在此处可以通过获取到的Msg值来判断
+                 * 给出用户提示注册成功 与否，以及判断是否用户名已经存在
+                 */
+                isSubmit = true;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +61,95 @@ public class LoginActivity extends AppCompatActivity {
         if(actionbar != null){
             actionbar.hide();
         }
+        /* 绑定组件 */
+        setFindViewId();
+        /* 添加监听器 */
+        setListeners();
 
+    }
+    /* 绑定组件 */
+    private void setFindViewId() {
+        intput_username = findViewById(R.id.input_username);
+        intput_password = findViewById(R.id.input_password);
+        btn_submit = findViewById(R.id.btn_submit);
+    }
+
+//    监听器
+    private void setListeners() {
+        btn_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                username = intput_username.getText().toString().trim();
+                password = intput_password.getText().toString().trim();
+                Map<String, Object> map1 = new HashMap<>();
+                map1.put("username",username);
+                map1.put("password",password);
+                //防止多次提交登录请求
+                if(isSubmit){
+                   // isSubmit = false;
+                    //通过okhttp发起post请求
+                    HttpUtil.sendPostRequest("http://192.168.0.6:8989/login", map1, new Callback() {
+                        @Override
+                        public void onFailure(Call call, IOException e) {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(LoginActivity.this,"失败",Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+
+                        @Override
+                        public void onResponse(Call call, final Response response) throws IOException {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    try {
+                                        if(response.body().string().equals("1"))
+                                        Toast.makeText(LoginActivity.this,"登录成功",Toast.LENGTH_SHORT).show();
+                                        else
+                                            Toast.makeText(LoginActivity.this,"登录失败",Toast.LENGTH_SHORT).show();
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            });
+                        }
+                    });
+                 //   postRequest(username,password);
+                }
+            }
+        });
+    }
+
+    private void postRequest(String username, String password) {
+        //建立请求表单，添加上传服务器的参数
+        RequestBody formBody = new FormBody.Builder()
+                .add("username",username)
+                .add("password",password)
+                .build();
+        //发起请求
+        final Request request = new Request.Builder()
+                .url("http://192.168.0.6:8989/login")
+                .post(formBody)
+                .build();
+        Toast.makeText(LoginActivity.this,"fsdf",Toast.LENGTH_SHORT).show();
+        //新建一个线程，用于得到服务器响应的参数
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Response response = null;
+                try {
+                    //回调
+                    response = client.newCall(request).execute();
+                    if (response.isSuccessful()){
+                    //将服务器响应的参数response.body().string())发送到hanlder中，并更新ui
+                        Log.d("",response.body().string());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 }
---
+++ app/src/main/java/com/example/jxqapp/util/HttpUtil.java
@@ -0,0 +1,66 @@
+package com.example.jxqapp.util;
+
+import java.io.File;
+import java.util.*;
+import okhttp3.*;
+
+/**
+ * Created by xgh007 on 2020/2/4.
+ * 这个类是专门进行发送Http请求的操作的，在本程序的任何地方都能调用这个类的方法来向后台发送请求
+ * 这个类中定义了2个方法，分别用于发送GET请求与POST请求
+ * 如果需要传递参数，要注意的的是：GET请求的参数直接是写在url上的，而POST请求的参数需要填在map中
+ * 这个类只负责发送请求，对响应或错误的具体处理，我们应该定义在okhttp3.Callback中，我们需要new一
+ * 个okhttp3.Callback对象，然后重写其的回调函数即可：“onResponse”和“onFailure” v0.1
+ * ------------------------------------
+ * 2020/2/11
+ * 增加了一个向后台上传文件的方法 v0.2
+ */
+public class HttpUtil {
+
+    /*// 发送GET请求
+    public static void sendGetRequest(String url, okhttp3.Callback callback){
+        OkHttpClient client = new OkHttpClient(); // 创建一个client
+        Request request = new Request.Builder().url(url).build(); // 组装一个Request对象
+        client.newCall(request).enqueue(callback); // 发送请求
+    }*/
+
+    // 发送POST请求
+    public static void sendPostRequest(String url, Map<String, Object> map, okhttp3.Callback callback){
+        OkHttpClient client = new OkHttpClient(); // 创建一个client
+        // POST请求的参数需要放在一个RequestBody对象中，它由FormBody.Builder建造者类来建造（建造者模式）
+        FormBody.Builder builder = new FormBody.Builder();
+        if(map != null){
+            // 不能直接把map转为RequestBody，必须遍历map的key，并逐一地往builder中添加对应的value
+            Set<String> keys = map.keySet();
+            for(String key : keys){ // 注意：RequestBody的参数只能是字符串类型的
+                String value = map.get(key).toString();
+                builder.add(key, value);
+            }
+        }
+        RequestBody requestBody = builder.build(); // 最后利用builder来生成一个RequestBody实例
+        // 组装一个Request对象（这次有额外传入RequestBody）
+        Request request = new Request.Builder().url(url).post(requestBody).build();
+        client.newCall(request).enqueue(callback); // 发送请求
+    }
+    // 处理文件上传 -- 每次发送一个文件
+    public static void sendUploadFileRequest(String url, File file, Map<String, Object> map, okhttp3.Callback callback){
+        OkHttpClient client = new OkHttpClient(); // 创建一个client
+        MediaType mediaType = MediaType.parse("application/octet-stream"); // 类型为八位字节流
+        RequestBody requestBody = RequestBody.create(mediaType, file); //把文件与类型放入请求体
+        MultipartBody.Builder builder = new MultipartBody.Builder(); // MultipartBody的建造者
+        builder.setType(MultipartBody.FORM);
+        builder.addFormDataPart("file", file.getName(), requestBody); // 设置参数名，后台接收一个“file”的文件参数
+        Set<String> keys = map.keySet();
+        for(String key : keys) { // 这里还可以接收一些String类型的参数
+            String value = map.get(key).toString();
+            builder.addFormDataPart(key, value);
+        }
+        MultipartBody multipartBody = builder.build(); // 建造一个请求体
+        Request request = new Request.Builder()
+                .url(url) // 指定url
+                .post(multipartBody)
+                .build();
+        client.newCall(request).enqueue(callback); // 让callback来处理回调结果
+    }
+}
+
